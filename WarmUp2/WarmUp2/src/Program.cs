24f0b345a735f7c8f099ed5eb7d3b7260f681e1c
Jonathan Lee <jonallee1@gmail.com>
2018-10-10 17:22:52 -0700

    completed compareTriplets function

--- WarmUp2/WarmUp2/src/Program.cs
+++ WarmUp2/WarmUp2/src/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace WarmUp2
+class WarmUp2
 {
-    class Program
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b)
     {
-        static void Main(string[] args)
+
+        int[] output = new int[2];
+        output[0] = 0;
+        output[1] = 0;
+
+        for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine("Hello World!");
+            if (a[i] < b[i]){
+                output[1] = output[1] + 1;
+            }
+            else if (a[i] == b[i])
+            {
+
+            }
+            else
+            {
+                output[0] = output[0] + 1;
+            }
         }
+
+        return output;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
