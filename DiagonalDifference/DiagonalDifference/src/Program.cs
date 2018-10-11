53d1bd323d1260322a64b2ec43656da28aa767d7
Jonathan Lee <jonallee1@gmail.com>
2018-10-10 18:47:03 -0700

    added src file

---
+++ DiagonalDifference/DiagonalDifference/src/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class DiagonalDifference
+{
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr)
+    {
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+        int size = arr.Length;
+
+        for (int i = 0; i < size; i++)
+        {
+
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
