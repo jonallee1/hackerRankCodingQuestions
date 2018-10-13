199cac0cb20fcf9c595a82e6ab00e592b2f2cc24
Jonathan Lee <jonallee1@gmail.com>
2018-10-12 19:47:49 -0700

    Completed EqualStacks problem

---
+++ HackerrankQuestions/HackerrankQuestions/src/DataStructures/EqualStacks.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerrankQuestions.src.DataStructures
+{
+    class EqualStacks
+    {
+         public int equalStacks(int[] h1, int[] h2, int[] h3)
+        {
+            //initalizing stacks from arrays
+            EqualStacks stacks = new EqualStacks();
+            Array.Reverse(h1);
+            Array.Reverse(h2);
+            Array.Reverse(h3);
+            Stack<int> stack1 = new Stack<int>(h1);
+            Stack<int> stack2 = new Stack<int>(h2);
+            Stack<int> stack3 = new Stack<int>(h3);
+            int totalblocks = stack1.Count() + stack2.Count() + stack3.Count();
+            int stack1height = checkStackHeight(stack1);
+            int stack2height = checkStackHeight(stack2);
+            int stack3height = checkStackHeight(stack3);
+
+
+            for (int i = 0; i < totalblocks; i++) //removes the top block from the tallest stack, will continue until all stacks are equal height, or there are no more blocks left
+            {
+
+                if (stack1height == stack2height && stack1height == stack3height)
+                {
+                    return stack1height;
+                }
+
+                else if (stack1height >= stack2height && stack1height >= stack3height )
+                {
+                    stack1height = stack1height - stack1.Peek();
+                    stack1.Pop();                   
+                }
+
+                else if (stack2height >= stack1height && stack2height >= stack3height)
+                {
+                    stack2height = stack2height - stack2.Peek();
+                    stack2.Pop();                    
+                }
+
+                else if (stack3height >= stack1height && stack3height >= stack2height)
+                {
+                    stack3height = stack3height - stack3.Peek();
+                    stack3.Pop();                   
+                }
+
+            }
+
+            return 0;
+
+        }
+
+
+        public int checkStackHeight(Stack<int> mystack)
+        {
+            int size = 0;
+            foreach (int item in mystack)
+            {
+                size = size + item;
+            }
+
+            return size;
+
+        }
+
+       
+
+
+
+    }
+}
--- HackerrankQuestions/HackerrankQuestions/src/Program.cs
+++ HackerrankQuestions/HackerrankQuestions/src/Program.cs
@@ -8,8 +8,25 @@ namespace HackerrankQuestions
         
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 19, 10, 12, 10, 24, 25, 22 };
-            nonDivisibleSubset.nonDivisibleSubsetFunction(4, arr);
+            /*int[] arr = new int[] { 19, 10, 12, 10, 24, 25, 22 };
+            nonDivisibleSubset.nonDivisibleSubsetFunction(4, arr);*/
+            int[] arr1 = new int[]
+            {
+                1,1,1,1,2
+            };
+
+            int[] arr2 = new int[]
+          {
+                3,7
+          };
+            int[] arr3 = new int[]
+          {
+               1,3,1
+          };
+            EqualStacks stacks = new EqualStacks();
+
+
+           Console.WriteLine(stacks.equalStacks(arr1, arr2, arr3));
             Console.ReadLine();
         }
     }
