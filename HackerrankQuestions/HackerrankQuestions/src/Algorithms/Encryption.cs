9bf465a3daa65088116490d75b6a5171d25a8c74
Jonathan Lee <jonallee1@gmail.com>
2018-10-13 17:59:13 -0700

    Implemented Encryption Function

---
+++ HackerrankQuestions/HackerrankQuestions/src/Algorithms/Encryption.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerrankQuestions.src.Algorithms
+{
+    class Encryption
+    {
+        public string encryption(string s)
+        {
+            String enc = "";
+            int L;
+            foreach (char c in s)
+            {
+                if (c == ' ')
+                { 
+
+                }
+                else
+                {
+                    enc = enc + c;
+                }
+                
+
+            }
+            L = enc.Length;
+            double rootL = Math.Sqrt(L);
+            double ceiling = Math.Ceiling(rootL);
+            double floor = Math.Floor(rootL);
+
+            Console.WriteLine("Root of L: " + rootL + "Ceiling: " + ceiling + "Floor: " + floor);
+
+            int column = 0;
+            int row = 0;
+
+            if (L > ceiling * floor )
+            {
+                column = Convert.ToInt32(ceiling);
+                row = Convert.ToInt32(ceiling);
+            }
+            else if (L > floor * floor && L <= floor * ceiling)
+            {
+                column = Convert.ToInt32(ceiling);
+                row = Convert.ToInt32(floor);
+            }
+            else
+            {
+                column = Convert.ToInt32(floor);
+                row = Convert.ToInt32(floor);
+            }
+
+            String output = "";
+            int count = 0;
+            int index = 0;
+            int numberreset = 0;
+            
+            for (int i = 0; i < row*column; i++)
+            {
+
+               
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Index: " + index);
+
+                if (i == 0)
+                {
+                    output = output + enc[index];
+                    index = index + column;
+                }
+                
+
+                else if (index < L-1)
+                {
+                    output = output + enc[index];
+                    index = index + column;
+                }
+
+                else if (index == L-1)
+                {
+
+                    Console.WriteLine("---------------------------------");
+                    output = output + enc[index];
+                    numberreset = numberreset + 1;
+                    index = numberreset;
+                   
+                }
+                else
+                {
+                    if (count == 0)
+                    {
+                        index = numberreset;
+                        output = output + enc[index];
+                        index = index + column;
+                    }
+                    
+                    else 
+                    {
+                        output = output + " ";
+                        count = -1;
+                        
+                        index = numberreset;
+                        numberreset = numberreset + 1;
+                    }
+                    
+                   
+                }
+                count++;
+                Console.WriteLine("Count: " + count);
+
+
+
+                if (count == row)
+                {
+                    output = output + " ";
+                    count = 0;
+                    numberreset = numberreset + 1;
+                    Console.WriteLine("output: " + output);
+
+                }
+
+               
+                
+                 
+                
+            }
+
+            Console.WriteLine("Output: " + output );
+            Console.WriteLine("Columns: " + column + "rows: " + row);
+            return output;
+
+        }
+
+
+    }
+}
--- HackerrankQuestions/HackerrankQuestions/src/Program.cs
+++ HackerrankQuestions/HackerrankQuestions/src/Program.cs
@@ -1,4 +1,5 @@
-﻿using HackerrankQuestions.src.DataStructures;
+﻿using HackerrankQuestions.src.Algorithms;
+using HackerrankQuestions.src.DataStructures;
 using System;
 
 namespace HackerrankQuestions
@@ -9,8 +10,11 @@ namespace HackerrankQuestions
         static void Main(string[] args)
         {
             //int[] arr = new int[] { 19, 10, 12, 10, 24, 25, 22 };
-            int[] arr = new int[] {1,7,2,4 };
-            nonDivisibleSubset.nonDivisibleSubsetFunction(3, arr);
+            //int[] arr = new int[] {1,7,2,4 };
+            //nonDivisibleSubset.nonDivisibleSubsetFunction(3, arr);
+            Encryption enc = new Encryption();
+            String test= "roqfqeylxuyxjfyqterizzkhgvngapvudnztsxeprfp";
+            Console.WriteLine(enc.encryption(test));
             /*int[] arr1 = new int[]
             {
                 1,1,1,1,2
