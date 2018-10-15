d9c01b6094af97b11074432365f58bd23b62aa39
Jonathan Lee <jonallee1@gmail.com>
2018-10-14 19:18:30 -0700

    Completed 3dSurfaceArea problem

---
+++ HackerrankQuestions/HackerrankQuestions/src/Algorithms/3DSurfaceArea.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerrankQuestions.src.Algorithms
+{
+    class _3DSurfaceArea
+    {
+        public int surfaceArea(int[][] A, int Height, int Width)
+        {
+            
+            int price = 0;
+            int[][] newA = new int[Height+2][ ];
+
+            for (int i = 0; i < Height + 2; i++)
+            {
+                newA[i] = new int[Width + 2];
+            }
+            for (int i = 1; i < Height + 1; i++)
+            {
+                for (int j = 1; j < Width + 1; j++)
+                {
+                    newA[i][j] = A[i - 1][j - 1];
+                }
+            }
+
+            for (int i = 0; i < Height + 2; i++)
+            {
+                for (int j = 0; j < Width + 2; j++)
+                {
+                    Console.Write(string.Format("{0} ", newA[i][ j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+
+
+       
+
+            for (int i = 1; i < Height+1; i++)
+            {
+                for (int j = 1; j < Width+1; j++)
+                {
+                    if (newA[i][ j] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        price = price + 2;    
+                        if(newA[i][ j] > newA[i-1][ j])
+                        {
+                            price = price + (newA[i][ j] - newA[i - 1][ j]);
+                        }
+                        if (newA[i][ j] > newA[i + 1][ j])
+                        {
+                            price = price + (newA[i][ j] - newA[i + 1][ j]);
+                        }
+                        if (newA[i][ j] > newA[i ][ j - 1])
+                        {
+                            price = price + (newA[i][ j] - newA[i][ j - 1]);
+                        }
+                        if (newA[i][ j] > newA[i][ j + 1])
+                        {
+                            price = price + (newA[i][ j] - newA[i][ j + 1]);
+                        }
+                    }
+
+
+
+
+                }
+
+
+
+            }
+
+            Console.WriteLine("price: " + price);
+
+            return price;
+
+        }
+
+
+    }
+}
--- HackerrankQuestions/HackerrankQuestions/src/Program.cs
+++ HackerrankQuestions/HackerrankQuestions/src/Program.cs
@@ -12,9 +12,21 @@ namespace HackerrankQuestions
             //int[] arr = new int[] { 19, 10, 12, 10, 24, 25, 22 };
             //int[] arr = new int[] {1,7,2,4 };
             //nonDivisibleSubset.nonDivisibleSubsetFunction(3, arr);
-            Encryption enc = new Encryption();
-            String test= "roqfqeylxuyxjfyqterizzkhgvngapvudnztsxeprfp";
-            Console.WriteLine(enc.encryption(test));
+            //Encryption enc = new Encryption();
+            //String test= "roqfqeylxuyxjfyqterizzkhgvngapvudnztsxeprfp";
+            //Console.WriteLine(enc.encryption(test));
+
+
+            _3DSurfaceArea sa = new _3DSurfaceArea();
+
+
+            int[][] array2D = new int[3][];
+            array2D[0] = new int[]{ 1, 3, 4 };
+            array2D[1] = new int[] { 2, 2, 3 };
+            array2D[2] = new int[] { 1, 2, 4 };
+            sa.surfaceArea(array2D, 3, 3);
+
+
             /*int[] arr1 = new int[]
             {
                 1,1,1,1,2
