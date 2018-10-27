eb5151257592a2f1d2664efb5e2d91954d490734
Jonathan Lee <jonallee1@gmail.com>
2018-10-26 19:36:30 -0700

    Is this BST Implementation

---
+++ HackerrankQuestions/HackerrankQuestions/src/DataStructures/IsThisaBST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerrankQuestions.src.DataStructures
+{
+    class IsThisaBST
+    {
+
+        public static void InOrder(Vector<Integer> arr, Node root)
+        {
+
+
+            if (root != null)
+            {
+                InOrder(arr, root.left);
+                arr.addElement(root.data);
+                InOrder(arr, root.right);
+
+
+
+            }
+        }
+
+        boolean checkBST(Node root)
+        {
+            Vector<Integer> arr = new Vector();
+            InOrder(arr, root);
+            for (int i = 0; i < arr.size() - 1; i++)
+            {
+                if (arr.get(i) >= arr.get(i + 1))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
